32ea2f0c8a4ea49bca51ea2c12365eb18360077d
Ekka-N <katiandrik.niki@gmail.com>
2025-03-15 15:07:02 +0300

    add header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { flexRowWrap, headerContainer, mainTitle, nav } from './headerStyles';
 
 export const Header = () => {
   return (
-    <div>
-      Саженцы из Поповки
-    </div>
+    <header className={headerContainer}>
+      <div className={`${flexRowWrap} mb-7`}>
+        <h1 className={mainTitle}>
+          Саженцы из Поповки
+        </h1>
+        <img alt="apple" src="img/apple.png" />
+      </div>
+
+      <div className={`${flexRowWrap}`}>
+        <nav className={`${nav}`}>
+          Главная
+        </nav>
+        <nav className={`${nav}`}>
+          Рекомендации по посадке и уходу
+        </nav>
+        <nav className={`${nav}`}>
+          Каталог сортов
+        </nav>
+      </div>
+    </header>
   );
 }
---
+++ src/components/header/headerStyles.ts
@@ -0,0 +1,4 @@
+export const headerContainer = 'min-h-20 py-5'
+export const flexRowWrap = 'flex items-center gap-4'
+export const mainTitle = 'text-4xl text-emerald-950 font-bold'
+export const nav = 'cursor-pointer pr-4'
